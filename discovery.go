70de3b71d69981f4541debfee1b40df54eb68c00
M <m@cognusion.com>
2017-07-01 09:30:38 -0400

    Fixes silly build-blocker, adds DiscoverAddrs() for a raw list of addresses

--- discovery.go
+++ discovery.go
@@ -30,6 +30,30 @@ func Discover(domain, service, scheme string) (urls []string, err error) {
 	return
 }
 
+// DiscoverAddrs does a DNS SRV lookup on the specified domain,
+// for the specified service, and returns a string array of addresses/names to use
+func DiscoverAddrs(domain, service string) (addresses []string, err error) {
+
+	errorChan := make(chan error)
+	discoChan := make(chan url.URL)
+
+	go DiscoverChan(domain, service, "http", discoChan, errorChan)
+
+	for {
+		select {
+		case err = <-errorChan:
+			if err.Error() == "Complete" {
+				err = nil
+			}
+			return
+		case u := <-discoChan:
+			addresses = append(addresses, u.Hostname())
+		}
+	}
+
+	return
+}
+
 // DiscoverChan does a DNS SRV lookup on the specified domain,
 // for the specified service, and streams url.URLs to use via discoChan,
 // and errors over errorChan, closing both when done. If errorChan
@@ -46,11 +70,11 @@ func DiscoverChan(domain, service, scheme string, discoChan chan url.URL, errorC
 	}
 
 	for _, srv := range addrs {
-		u := &url.URL{
+		u := url.URL{
 			Scheme: scheme,
 			Host:   net.JoinHostPort(srv.Target, fmt.Sprintf("%d", srv.Port)),
 		}
-		discoChan <- &u
+		discoChan <- u
 	}
 
 	errorChan <- fmt.Errorf("Complete")
